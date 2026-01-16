3f7b5636c84314c990e284224d401c78c9683257
RobsonSousa <robson.ferreira123@hotmail.com>
2026-01-16 12:56:34 -0300

    chore: ajuste nos tipos de retorno dos métodos de procura de usuários

--- cadastro-usuario/src/main/java/com/java/cadastro_usuario/Controller/UserController.java
+++ cadastro-usuario/src/main/java/com/java/cadastro_usuario/Controller/UserController.java
@@ -4,6 +4,8 @@ import com.java.cadastro_usuario.Model.User;
 import com.java.cadastro_usuario.Model.UserDataSave;
 import com.java.cadastro_usuario.Service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,18 +19,18 @@ public class UserController {
     @PostMapping("/save")
     public ResponseEntity<Void> saveUser(@RequestBody UserDataSave user){
         userService.saveUser(new User(user));
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
     @GetMapping("/{id}")
-    public ResponseEntity<UserDataSave> findUserById(@PathVariable Integer id){
-        userService.getUserById(id);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<User> findUserById(@PathVariable Integer id){
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("/email/{email}")
     public ResponseEntity<User> findUserByEmail(@PathVariable String email){
-        userService.getUserByEmail(email);
-        return ResponseEntity.ok().build();
+        User user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user);
     }
 
 }
